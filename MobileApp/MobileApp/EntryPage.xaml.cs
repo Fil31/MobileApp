98f8de6b330daaf225fe4fc59856dfd5cd1c61e6
opilane <opilane@HP-CZC2349HTC>
2023-02-10 11:26:00 +0200

    proshma

--- MobileApp/MobileApp/EntryPage.xaml.cs
+++ MobileApp/MobileApp/EntryPage.xaml.cs
@@ -13,13 +13,20 @@ namespace MobileApp
     public partial class EntryPage : ContentPage
     {
         Editor ed;
+        Label lbl;
         public EntryPage()
         {
-            ed= new Editor
+            ed = new Editor
             {
-                Placeholder="Sisesta siia tekst",
-                BackgroundColor=Color.MidnightBlue,
-                TextColor=Color.White
+                Placeholder = "Sisesta siia tekst",
+                BackgroundColor = Color.MidnightBlue,
+                TextColor = Color.White
+            };
+            ed.TextChanged += Ed_TextChanged;
+            lbl = new Label
+            {
+                Text = "Mingi text",
+                TextColor = Color.White
             };
             Button Start_btn = new Button
             {
@@ -27,18 +34,31 @@ namespace MobileApp
                 TextColor = Color.Turquoise,
                 BackgroundColor = Color.DarkRed
             };
-            StackLayout st= new StackLayout
+            StackLayout st = new StackLayout
             {
                 Orientation = StackOrientation.Vertical,
-                Children = { ed, Start_btn },
+                Children = { ed, lbl, Start_btn },
                 BackgroundColor = Color.Orange
             };
             Content = st;
-            Start_btn.Clicked += Start_btn_clicked;
+            Start_btn.Clicked += Start_btn_Clicked;
         }
-        private async void Start_btn_clicked(object sender, EventArgs e)
+        private async void Start_btn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new StartPage());
+        }
+        int i = 0;
+        private void Ed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lbl.Text = ed.Text;
+            char key = e.NewTextValue?.LastOrDefault() ?? ' ';
+
+            if (key == 'A')
+            {
+                i++;
+                lbl.Text = key.ToString() + ": " + i;
+            }
+
         }
     }
 }
--- MobileApp/MobileApp/StartPage.xaml.cs
+++ MobileApp/MobileApp/StartPage.xaml.cs
@@ -17,22 +17,44 @@ namespace MobileApp
             //initializeComponent();
             Button Entry_btn = new Button
             {
-                Text = "Ava leht",
+                Text = "Open main page",
+                TextColor = Color.Turquoise,
+                BackgroundColor = Color.DarkRed
+            };
+            Button Timer_btn = new Button
+            {
+                Text = "Open timer page",
+                TextColor = Color.Turquoise,
+                BackgroundColor = Color.DarkRed
+            };
+            Button BoxView_btn = new Button
+            {
+                Text = "Open boxview page",
                 TextColor = Color.Turquoise,
                 BackgroundColor = Color.DarkRed
             };
             StackLayout st = new StackLayout
             {
                 Orientation = StackOrientation.Vertical,
-                Children = { Entry_btn },
+                Children = { Entry_btn, Timer_btn, BoxView_btn},
                 BackgroundColor = Color.Gold
             };
             Content= st;
             Entry_btn.Clicked += Entry_btn_clicked;
+            Timer_btn.Clicked += Timer_btn_clicked;
+            BoxView_btn.Clicked += BoxView_btn_clicked;
         }
         private async void Entry_btn_clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new EntryPage());
+        }
+        private async void BoxView_btn_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BoxViewPage());
+        }
+        private async void Timer_btn_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Timer_Page());
         }
     }
 }
